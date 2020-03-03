6fa2d07f7bb7eb8341b345021fa5a37eb0a7b5ed
syedmoinn36 <61343723+syedmoinn36@users.noreply.github.com>
2020-03-03 10:21:35 +0530

    Add files via upload

---
+++ dowhile.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int counter=1;
+do
+{
+printf("HELLO\n");
+counter++;
+}
+while(counter<=0); 
+}
---
+++ for2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+int counter,i;
+for(;;)
+{
+printf("%d%d\n",counter,i);
+}
+}
